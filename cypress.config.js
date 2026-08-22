97a916d57af8e19dbd8f62b3da34577c14525bd8
Raytek <raytek.net@gmail.com>
2026-08-22 16:51:22 -0300

    chore: initialize Cypress project

---
+++ cypress.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  allowCypressEnv: false,
+
+  e2e: {
+    specPattern: [
+      'cypress/e2e/**/*.cy.js',
+      'cypress/api/**/*.cy.js',
+    ],
+    baseUrl: 'https://front.serverest.dev',
+    env: {
+      apiUrl: 'https://serverest.dev',
+    },
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
