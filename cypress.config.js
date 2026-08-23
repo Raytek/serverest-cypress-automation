5fbe36ccccbf6e872f92081fb8bbd348c292e714
Raytek <raytek.net@gmail.com>
2026-08-23 07:54:01 -0300

    test: add Mochawesome test reporting

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,13 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports/mochawesome',
+    overwrite: false,
+    html: false,
+    json: true
+  },
   allowCypressEnv: false,
   expose: {
     apiUrl: 'https://serverest.dev'
